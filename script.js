d0581e6b0c6511fde3f1fe26c8d182ada90512f7
kcbobbe <kcbobbe@gmail.com>
2018-05-11 14:51:32 -0400

    Made it so that my coin spawns in a random spot. started trying to implement collision detection

--- script.js
+++ script.js
@@ -6,24 +6,44 @@ class Game{
         this.screen = canvas.getContext("2d");
         this.player = new Player(this);
         this.coin = new Coin(this);
+//collision
+        this.bodies = [];
+        this.bodies = this.bodies.concat(this.player);
+        this.bodies = this.bodies.concat(this.coin);
+        // var self = this;
+       
     }
     draw(){
         this.screen.clearRect(0,0, this.canvas.width, this.canvas.height);
         this.screen.strokeStyle="#FF0000";
         this.screen.lineWidth = 10;
         this.screen.strokeRect(150,150,200,200);
-        this.player.draw();
-        this.coin.draw();
+        for (var i = 0; i < this.bodies.length; i++) {
+            (this.bodies[i]).draw();
+        }
+        // this.player.draw();
+        // this.coin.draw();
     }
 
     tick(){
         this.update();
         this.draw();
-        console.log("tick")
+        
         requestAnimationFrame(this.tick.bind(this));
     }
     update(){
         this.player.update();
+        //new
+        // var notCollidingWithAnything = function(b1) {
+        //     return self.bodies.filter(function(b2) {return colliding(b1, b2);}).length === 0;
+        // };
+        //     this.bodies = this.bodies.filter(notCollidingWithAnything);
+        //     for (var i = 0; i < this.bodies.length; i++) {
+        //     this.bodies[i].update();
+        // }
+    }
+    addBody(body){
+        this.bodies.push(body);
     }
 }
 
@@ -65,15 +85,29 @@ class Coin {
         this.game = game;
         this.screen = game.screen;
         // this.location={x:230,y:230, size:40}
+        this.leftX=(Math.floor(Math.random()*155))+155;
+        this.leftY=(Math.floor(Math.random()*155))+155;
     }
     draw(){
         this.screen.fillStyle="#FFDD00";
         var size = 10;
-        var leftX=(200);
-        var leftY=(200);
+
+        // var leftX=(200);
+        // var leftY=(200);
         // var leftX=(Math.floor(Math.random()*300));
         // var leftY=(Math.floor(Math.random()*300));
-        this.screen.fillRect(leftX,leftY,size,size);
+        this.screen.fillRect(this.leftX,this.leftY,size,size);
+    }
+}
+
+class colliding{
+    constructor(b1, b2) {
+        return !( b1 === b2 ||
+        b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+        b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+        b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+        b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+        );
     }
 }
 
