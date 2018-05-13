3e520d4f13b1b13f6df06ebe39dcd1b48a6667e7
kcbobbe <kcbobbe@gmail.com>
2018-05-13 17:52:54 -0400

    Made it so that the red blocks move in from the top and bottom. When a red block hits the player, the score resets to 0.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("hi");
-var count=0;
+// var count=0;
 class Game{
     constructor(canvas){
         this.canvas = canvas;
@@ -7,12 +7,7 @@ class Game{
         this.player = new Player(this);
         this.coin = new Coin(this);
         this.badCoin = new BadCoin(this);
-//collision
-        this.bodies = [];
-        this.bodies = this.bodies.concat(this.player);
-        this.bodies = this.bodies.concat(this.coin);
-        // var self = this;
-       
+        this.badCoinB = new BadCoinB(this);
     }
     draw(){
         this.screen.clearRect(0,0, this.canvas.width, this.canvas.height);
@@ -25,6 +20,9 @@ class Game{
         this.player.draw();
         this.coin.draw();
         this.badCoin.draw();
+        if (document.getElementById("counter").innerText > 5){
+            this.badCoinB.draw();
+        }
         
     }
 
@@ -36,49 +34,40 @@ class Game{
     }
     update(){
         this.player.update();
-        // this.coin.update();
-        //new
+        this.badCoin.location.y +=4
+        if (this.badCoin.location.y > 330 ){
+            this.badCoin.update();
+        }
+
+        this.badCoinB.location.y -=7;
+        if (document.getElementById("counter").innerText > 5 && this.badCoinB.location.y < 155){
+            this.badCoinB.update();
+        }
+      
+        
         if (colliding(this.coin.location, this.player.location)){
-            console.log(this.coin.location);
-            console.log(this.player.location);
-            // var counter=document.getElementById("counter");
-            // var count="0";
-            // counter.innerText=count;
-           
-            counter=document.getElementById("counter");
-            count= count+1;
-            counter.innerText=count;
-            console.log(count)
+            var count = document.getElementById("counter");
+            count.innerText = parseInt(count.innerText)+1;
+            console.log(count);
             this.coin.update();
             
         }
 
         if (colliding(this.badCoin.location, this.player.location)){
-            console.log(this.badCoin.location);
-            console.log(this.player.location);
-            // var counter=document.getElementById("counter");
-            // var count="0";
-            // counter.innerText=count;
-           
-            counter=document.getElementById("counter");
-            count= count-1;
-            counter.innerText=count;
+            var count = document.getElementById("counter");
+            count.innerText = 0;
             console.log(count)
             this.badCoin.update();
             
         }
 
-        // var notCollidingWithAnything = function(b1) {
-        //      return this.bodies.filter(function(b2) {return colliding(b1, b2);}).length === 0;
-
-        //  };
-        //      this.bodies = this.bodies.filter(notCollidingWithAnything);
-        //      for (var i = 0; i < this.bodies.length; i++) {
-        //      this.bodies[i].update();
-    }
-    
-    addBody(body){
-        this.bodies.push(body);
+        if (colliding(this.badCoinB.location, this.player.location)){
+            var count = document.getElementById("counter");
+            count.innerText = 0;
+            console.log(count)
+            this.badCoinB.update();
+            
+        }
     }
 }
 
@@ -87,28 +76,43 @@ class Player{
         this.game = game;
         this.screen = game.screen;
         this.keyboarder = new Keyboarder();
-        this.location={x:230,y:230, size:40}
+        this.location={x:230,y:230, sizeX:80, sizeY:20}
     }
     draw(){
-        this.screen.fillStyle="#FFFF00";
-        var size = this.location.size;
+        this.screen.fillStyle="#FF8800";
+        var sizeX = this.location.sizeX;
+        var sizeY = this.location.sizeY;
         var leftX=this.location.x
         var leftY=this.location.y
-        this.screen.fillRect(leftX,leftY,size,size);
+        this.screen.fillRect(leftX,leftY,sizeX,sizeY);
     }
     update(){
-        if(this.keyboarder.isDown(Keyboarder.KEYS.LEFT) && this.location.x>155){
-            this.location.x -=1;
+
+        if(this.keyboarder.isDown(Keyboarder.KEYS.LEFT) && this.keyboarder.isDown(Keyboarder.KEYS.SPACE) && this.location.x>155){
+            this.location.x -= 7;
 
         }
-        else if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)&& this.location.x<305){
-            this.location.x += 1;
+        else if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)&& this.keyboarder.isDown(Keyboarder.KEYS.SPACE) && this.location.x<264){
+            this.location.x += 7;
         }
-        else if (this.keyboarder.isDown(Keyboarder.KEYS.DOWN)&& this.location.y<305){
-            this.location.y += 1;
+        else if (this.keyboarder.isDown(Keyboarder.KEYS.DOWN)&& this.keyboarder.isDown(Keyboarder.KEYS.SPACE) && this.location.y<325){
+            this.location.y += 7; 
+        }
+        else if (this.keyboarder.isDown(Keyboarder.KEYS.UP)&& this.keyboarder.isDown(Keyboarder.KEYS.SPACE) && this.location.y>155){
+            this.location.y -= 7;
+        }
+        else if(this.keyboarder.isDown(Keyboarder.KEYS.LEFT) && this.location.x>155){
+            this.location.x -= 3;
+
+        }
+        else if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)&& this.location.x<264){
+            this.location.x += 3;
+        }
+        else if (this.keyboarder.isDown(Keyboarder.KEYS.DOWN)&& this.location.y<325){
+            this.location.y += 3;
         }
         else if (this.keyboarder.isDown(Keyboarder.KEYS.UP)&& this.location.y>155){
-            this.location.y -= 1;
+            this.location.y -= 3;
         }
 
 
@@ -119,22 +123,13 @@ class Coin {
     constructor (game) {
         this.game = game;
         this.screen = game.screen;
-        // this.location={x:230,y:230, size:40}
         this.location={x: (Math.floor(Math.random()*155)+155),y:((Math.floor(Math.random()*155))+155)}
-        // this.leftX=(Math.floor(Math.random()*155))+155;
-        // this.leftY=(Math.floor(Math.random()*155))+155;
     }
     draw(){
         this.screen.fillStyle="#FFDD00";
-        var size = 20;
-
-        // var leftX=(200);
-        // var leftY=(200);
-        // var leftX=(Math.floor(Math.random()*300));
-        // var leftY=(Math.floor(Math.random()*300));
-        //this.screen.fillRect(this.leftX,this.leftY,size,size);
-        this.screen.fillRect(this.location.x,this.location.y,size,size);
-
+        var sizeX = 20;
+        var sizeY = 20;
+        this.screen.fillRect(this.location.x,this.location.y,sizeX,sizeY);
     }
     update(){
         this.location.x=(Math.floor(Math.random()*155))+155;
@@ -144,29 +139,42 @@ class Coin {
 
 
 class BadCoin {
-    constructor (game) {
+    constructor (game, direction) {
         this.game = game;
         this.screen = game.screen;
-        // this.location={x:230,y:230, size:40}
-        this.location={x: (Math.floor(Math.random()*155)+155),y:((Math.floor(Math.random()*155))+155)}
-        // this.leftX=(Math.floor(Math.random()*155))+155;
-        // this.leftY=(Math.floor(Math.random()*155))+155;
+        this.location={x: (Math.floor(Math.random()*155)+155),y:(0)}
+        
     }
     draw(){
         this.screen.fillStyle="#FF0000";
-        var size = 20;
-
-        // var leftX=(200);
-        // var leftY=(200);
-        // var leftX=(Math.floor(Math.random()*300));
-        // var leftY=(Math.floor(Math.random()*300));
-        //this.screen.fillRect(this.leftX,this.leftY,size,size);
-        this.screen.fillRect(this.location.x,this.location.y,size,size);
+        var sizeX = 20;
+        var sizeY = 20;
+        this.screen.fillRect(this.location.x,this.location.y,sizeX,sizeY);
 
     }
     update(){
         this.location.x=(Math.floor(Math.random()*155))+155;
-        this.location.y=(Math.floor(Math.random()*155))+155;
+        this.location.y=(0);
+    }
+}
+
+
+class BadCoinB {
+    constructor (game) {
+        this.game = game;
+        this.screen = game.screen;
+        this.location={x: (Math.floor(Math.random()*155)+155),y:(500)}
+    }
+    draw(){
+        this.screen.fillStyle="#FF0000";
+        var sizeX = 20;
+        var sizeY = 20;
+        this.screen.fillRect(this.location.x,this.location.y,sizeX,sizeY);
+
+    }
+    update(){
+        this.location.x=(Math.floor(Math.random()*155))+155;
+        this.location.y=(500);
     }
 }
 
@@ -176,14 +184,14 @@ function colliding(b1, b2) {
         var b1centerX= b1.x+(10)
         var b1centerY= b1.y+(10)
     
-        var b2centerX= b2.x+(b2.size/2)
-        var b2centerY= b2.y+(b2.size/2)
+        var b2centerX= b2.x+(b2.sizeX/2)
+        var b2centerY= b2.y+(b2.sizeY/2)
     
         return !( b1 === b2 ||
-            b1centerX + 10 < b2centerX - b2.size / 2 ||
-            b1centerY + 10 < b2centerY - b2.size / 2 ||
-            b1centerX - 10 > b2centerX + b2.size / 2 ||
-            b1centerY - 10 > b2centerY + b2.size / 2
+            b1centerX + 10 < b2centerX - b2.sizeX / 2 ||
+            b1centerY + 10 < b2centerY - b2.sizeY / 2 ||
+            b1centerX - 10 > b2centerX + b2.sizeX / 2 ||
+            b1centerY - 10 > b2centerY + b2.sizeY / 2
         );
     
         // return !( b1 === b2 ||
@@ -232,9 +240,7 @@ class Keyboarder {
     }                      
   }
   
-  Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, S: 83 };
-
-
+  Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, SPACE: 32 };
   var canvas = document.getElementById("myCanvas");
 
   //window.addEventListener('load', function(){
